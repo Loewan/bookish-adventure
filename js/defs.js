186bfff7cbd19649c02c119480486c2e6b3b9874
Loewan Lafleur <loewanlafleur@Kalis-MacBook-Air.local>
2022-02-25 15:52:48 -0700

    First commit, constants and init functions

---
+++ js/defs.js
@@ -0,0 +1,9 @@
+const PIECES = { EMPTY:0, wP:1, wN:2, wB:3, wR:4, wQ:5, wK:6, bp:7, bN:8, bB:9, bR:10, bQ:11, bK:12 };
+const BRD_SQ_NUM = 120;
+const FILES = {A:0, B:1, C:2, D:3, E:4, F:5, G:6, H:7, NONE:8};
+const RANKS = {R1:0, R2:1, R3:2, R4:3, R5:4, R6:5, R7:6, R8:7, NONE:8};
+const COLORS = { white: 0, black: 1, both: 2};
+const SQUARES = { A1:21, B1:22, C1:23, D1:24, E1:25, F1:26, G1:27, H1:28,
+                  A8:91, B8:92, C8:93, D8:94, E8:95, F8:96, G8:97, H8:98,
+                  NO_SQ:99, OFFBOARD:100 };
+const BOOL = { TRUE: 1, FALSE: 0 };
---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  init();
+  console.log("Main INIT called")
+});
+
+function init() {
+  console.log("init() called")
+}
