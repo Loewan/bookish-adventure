9b3c9c5e0d50810d1bfdeca9db978fbc4ecfd39a
Loewan Lafleur <loew.dev@gmail.com>
2022-04-03 18:10:22 -0600

    Create SqAttacked function and printout for debugging

--- js/board.js
+++ js/board.js
@@ -121,7 +121,7 @@ function UpdateListsMaterials() {
   }
 
   PrintPieceLists();
-  
+
 }
 
 function ResetBoard() {
@@ -234,6 +234,7 @@ function ParseFen(fen) {
 
   GameBoard.posKey = GeneratePosKey();
   UpdateListsMaterials();
+  PrintSqAttacked();
 }
 
 /*
@@ -255,3 +256,89 @@ for(pceNum = 0; pceNum < GameBoard.pceNum[wP]; ++pceNum) {
 
 
 */
+
+function PrintSqAttacked() {
+	
+	var sq,file,rank,piece;
+
+	console.log("\nAttacked:\n");
+	
+	for(rank = RANKS.R8; rank >= RANKS.R1; rank--) {
+		var line =((rank+1) + "  ");
+		for(file = FILES.A; file <= FILES.H; file++) {
+			sq = FR2SQ(file,rank);
+			if(SqAttacked(sq, GameBoard.side) == BOOL.TRUE) piece = "X";
+			else piece = "-";
+			line += (" " + piece + " ");
+		}
+		console.log(line);
+	}
+	
+	console.log("");
+	
+}
+
+function SqAttacked(sq, side) {
+  var pce;
+  var t_sq;
+  var index;
+
+  if(side == COLORS.white) {
+    if (GameBoard.pieces[sq-11] == PIECES.wP || GameBoard.pieces[sq-9] == PIECES.wP) {
+      return BOOL.TRUE;
+    }
+  } else {
+    if (GameBoard.pieces[sq+11] == PIECES.bP || GameBoard.pieces[sq+9] == PIECES.bP) {
+      return BOOL.TRUE;
+    }
+  }
+
+  for(index = 0; index < 8; index++)  {
+    pce = GameBoard.pieces[sq + KnDir[index]];
+    if(pce != SQUARES.OFFBOARD && PieceCol[pce] == side && PieceKnight[pce] == BOOL.TRUE) {
+      return BOOL.TRUE;
+    }
+  }
+
+  for(index = 0; index < 4; index++) {
+    dir = RkDir[index];
+    t_sq = sq + dir;
+    pce = GameBoard.pieces[t_sq];
+    while (pce != SQUARES.OFFBOARD) {
+      if(pce != PIECES.EMPTY) {
+        if(PieceRookQueen[pce] == BOOL.TRUE && PieceCol[pce] == side) {
+          return BOOL.TRUE;
+        }
+        break;
+      }
+      t_sq += dir;
+      pce = GameBoard.pieces[t_sq];
+    }
+  }
+
+  for(index = 0; index < 4; index++) {
+    dir = BiDir[index];
+    t_sq = sq + dir;
+    pce = GameBoard.pieces[t_sq];
+    while (pce != SQUARES.OFFBOARD) {
+      if(pce != PIECES.EMPTY) {
+        if(PieceBishopQueen[pce] == BOOL.TRUE && PieceCol[pce] == side) {
+          return BOOL.TRUE;
+        }
+        break;
+      }
+      t_sq += dir;
+      pce = GameBoard.pieces[t_sq];
+    }
+  }
+
+  for(index = 0; index < 8; index++)  {
+    pce = GameBoard.pieces[sq + KiDir[index]];
+    if(pce != SQUARES.OFFBOARD && PieceCol[pce] == side && PieceKing[pce] == BOOL.TRUE) {
+      return BOOL.TRUE;
+    }
+  }
+
+  return BOOL.FALSE;
+
+}
